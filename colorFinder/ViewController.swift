8de0eeca0ad3b03e861e685e252812beb6b52c4b
廖昱晴 <pulin.liao@ctbcsec.com>
2021-02-25 13:36:42 +0800

    - init colorFinder app

--- colorFinder/ViewController.swift
+++ colorFinder/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redText: UILabel!
+    @IBOutlet weak var greenText: UILabel!
+    @IBOutlet weak var blueText: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var redValue = 255
+    var greenValue = 255
+    var blueValue = 255
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redText.text = "R:\(redValue)"
+        greenText.text = "G:\(greenValue)"
+        blueText.text = "B:\(blueValue)"
+        redSlider.value = Float(redValue)
+        greenSlider.value = Float(greenValue)
+        blueSlider.value = Float(blueValue)
+        changedBackgroundColor()
     }
 
-
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = Int(sender.value)
+        changedBackgroundColor()
+        redText.text = "R:\(redValue)"
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        changedBackgroundColor()
+        greenText.text = "G:\(greenValue)"
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        changedBackgroundColor()
+        blueText.text = "B:\(blueValue)"
+    }
+    
+    func changedBackgroundColor() {
+        view.backgroundColor = UIColor(red: CGFloat(redValue) / 255, green: CGFloat(greenValue) / 255, blue: CGFloat(blueValue) / 255, alpha: 1)
+    }
+    
 }
 
